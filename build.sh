240f8940a04a8970f6d3ec85d2460265e2c28eeb
Sean McGoff <seentheshadow@gmail.com>
2022-06-23 02:36:11 -0400

    Build sh file

---
+++ build.sh
@@ -0,0 +1,11 @@
+clang++ \
+-Wall \
+-std=c++17 \
+--target=wasm32 \
+-Ofast \
+--no-standard-libraries \
+-Wl,--export=processAudio \
+-Wl,--no-entry \
+-Wl,--import-memory \
+-o main.wasm \
+main.cpp

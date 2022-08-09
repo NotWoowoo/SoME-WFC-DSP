ee4b7c6ca93aa9e21c24ec0429942f7d3e7e08fc
notwoowoo <gamerwoowoo6@gmail.com>
2022-08-09 16:40:24 -0500

    error color is better

--- js/dsp.js
+++ js/dsp.js
@@ -34,7 +34,7 @@ function setDSP(js){
         audioWorkletNode.connect(dspAudioContextAnalyzer)
 
         document.getElementById('editor-err').innerHTML = ''
-        audioWorkletNode.port.onmessage = e => document.getElementById('editor-err').innerHTML = '<div style="color:red;"><b>ERROR: </b>' + e.data + '</div>'
+        audioWorkletNode.port.onmessage = e => document.getElementById('editor-err').innerHTML = '<div class="text-red-700 dark:text-red-300"><b>ERROR: </b>' + e.data + '</div>'
 
         return audioContext
     })(js)
